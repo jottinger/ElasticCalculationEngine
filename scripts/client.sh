fa494e3a01c141c219c82410267370c63e7a86a6
Joseph Ottinger <joeo@enigmastation.com>
2011-07-25 14:03:43 -0400

    First stab at the console client startup script

---
+++ scripts/client.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! -n "$GSHOME" ] ; then
+    echo Environment variable GSHOME not defined.
+    echo Please set GSHOME to the home directory of your GigaSpaces installation.
+    exit 1
+fi
+
+#export CLASSPATH=
+#export SEPARATOR=
+#for i in $GSHOME/lib/required/*; do
+#   export CLASSPATH=${CLASSPATH}${SEPARATOR}$i
+#   export SEPARATOR=:
+#done
+
+java -cp "$GSHOME/lib/required/*":../ece-client/target/classes org.openspaces.ece.client.ConsoleClient
