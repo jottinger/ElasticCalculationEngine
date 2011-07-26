9c5e51d2512c1e42f309d4c24042f1b043d3aae0
Joseph Ottinger <joeo@enigmastation.com>
2011-07-26 12:05:41 -0400

    Updating README and Windows execution script for client

---
+++ scripts/client.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+
+echo %CD%
+echo %~dp0
+
+if not "%GSHOME%" == "" goto :gsdefined
+echo Please define the GSHOME environment variable.
+goto :exit
+
+:gsdefined
+set GSCLASSPATH=%GSHOME%\lib\required\*
+set BASECLASSPATH=%~dp0..\ece-client\target\ece-client-1.0.jar
+echo %BASECLASSPATH% 
+java -cp "%GSCLASSPATH%;%BASECLASSPATH%" org.openspaces.ece.client.ConsoleClient
+
+:exit
