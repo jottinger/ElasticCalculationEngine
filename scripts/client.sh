e1c100c568aa77ea9797028850258f00ce9d3ffe
Joseph Ottinger <joeo@enigmastation.com>
2011-07-26 07:34:09 -0400

    Adding cygwin support for client.sh

--- scripts/client.sh
+++ scripts/client.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
+# turn off wildcards
+set -f
+
+# make sure GSHOME is defined
 if [ ! -n "$GSHOME" ] ; then
     echo Environment variable GSHOME not defined.
     echo Please set GSHOME to the home directory of your GigaSpaces installation.
     exit 1
 fi
 
-#export CLASSPATH=
-#export SEPARATOR=
-#for i in $GSHOME/lib/required/*; do
-#   export CLASSPATH=${CLASSPATH}${SEPARATOR}$i
-#   export SEPARATOR=:
-#done
+# establish where the script is running from, so we can use relative paths
+SCRIPTDIR=`dirname $0`
 
-java -cp "$GSHOME/lib/required/*":../ece-client/target/classes org.openspaces.ece.client.ConsoleClient
+# detect Cygwin
+cygwin=false;
+case "`uname`" in
+  CYGWIN*) cygwin=true;
+esac
+  
+
+export BASECLASSPATH=$SCRIPTDIR/../ece-client/target/ece-client-1.0.jar
+export GSCLASSPATH="$GSHOME/lib/required/"
+export SEPARATOR=":"
+if $cygwin; then
+   export SEPARATOR=";"
+   export GSCLASSPATH=`cygpath --windows $GSCLASSPATH`
+   export BASECLASSPATH=`cygpath --windows $BASECLASSPATH`
+fi
+
+java -cp "$GSCLASSPATH*$SEPARATOR$BASECLASSPATH" org.openspaces.ece.client.ConsoleClient
